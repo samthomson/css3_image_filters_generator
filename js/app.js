96c4f922bc19a35eedd0b0900be5ab22592b3fbb
samthomson <samt@samt.st>
2014-06-26 20:47:39 +0200

    css injection, bit more logic in2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,54 @@
 function FilterCtrl($scope) {
-  $scope.filters = [];
- 
+  /*
+  blur
+  brightness
+  contrast
+  url
+  drop-shadow
+  grayscale
+  hue-rotate
+  invert
+  opacity
+  sepla
+  */
+  $scope.name = "sam";
+  $scope.filter_prefix = "-webkit-filter:";
+  $scope.filters = [
+    {
+      "name":"blur",
+      "value":1,
+      "pre":"blur(",
+      "post":"px)"
+    },
+    {
+      "name":"brightness",
+      "value":60,
+      "pre":"brightness(",
+      "post":"%)"
+    },
+    {
+      "name":"contrast",
+      "value":100,
+      "pre":"contrast(",
+      "post":"%)"
+    }
+  ];
  
   $scope.css = function() {
-    var s_css_rules = 'css?';
+    // return raw css styles for injection
+    var s_css_rules = $scope.filter_prefix;
     angular.forEach($scope.filters, function(filter) {
-      //s_css_rules += filter.value ? 0 : 1;
+      s_css_rules += filter.pre + filter.value + filter.post + ' '
     });
-    return s_css_rules;
+    return s_css_rules + ';';
+  }
+ 
+  $scope.css_formatted = function() {
+    // return formatted css for putting into a css file
+    var s_css_formatted = '.css3_image_filter{';
+    
+    s_css_formatted += css();
+    
+    return s_css_formatted + '}';
   };
 }
