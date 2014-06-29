eaa464eb0f71257130d3b7f0c5f66028000cfe3d
samthomson <samt@samt.st>
2014-06-29 18:06:43 +0200

    tidyed up ui, intelligent css generation, slider ui

--- js/app.js
+++ js/app.js
@@ -14,49 +14,56 @@ function FilterCtrl($scope) {
   opacity
   sepla
   */
-  $scope.image_url = "http://farm4.staticflickr.com/3915/14503838952_748f7f441d_c.jpg";
+  $scope.image_url = "http://0.cdn.samt.st/lightbox/0de2fe841f9c4c969b9911eb6a8b5c17a343c196.jpg";
 
   $scope.filter_prefix = "-webkit-filter:";
   $scope.filters = [
     {
       "name":"grayscale",
       "value":0,
+      "default":0,
       "pre":"grayscale(",
       "post":"%)"
     },
     {
       "name":"sepia",
       "value":0,
+      "default":0,
       "pre":"sepia(",
       "post":"%)"
     },
     {
       "name":"saturate",
-      "value":50,
+      "value":100,
+      "default":100,
       "pre":"saturate(",
       "post":"%)"
     },
     {
       "name":"invert",
       "value":0,
+      "default":0,
       "pre":"invert(",
       "post":"%)"
     },
     {
       "name":"brightness",
       "value":100,
+      "default":100,
       "pre":"brightness(",
       "post":"%)"
     },
     {
       "name":"blur",
       "value":0,
+      "default":0,
       "pre":"blur(",
       "post":"px)"
     },
     {
       "name":"contrast",
       "value":100,
+      "default":100,
       "pre":"contrast(",
       "post":"%)"
     }
@@ -66,7 +73,8 @@ function FilterCtrl($scope) {
     // return raw css styles for injection
     var s_css_rules = $scope.filter_prefix;
     angular.forEach($scope.filters, function(filter) {
-      s_css_rules += filter.pre + filter.value + filter.post + ' '
+      if(filter.value !== filter.default)
+        s_css_rules += filter.pre + filter.value + filter.post + ' '
     });
     return s_css_rules + ';';
   }
